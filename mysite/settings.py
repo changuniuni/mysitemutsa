6cf04e2e729735a042290d6827b00e1995c6c032
changchang <changchang@unist.ac.kr>
2019-07-29 20:23:02 +0900

    settings.py update

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x#jghm@4&dyl&sza++#s5vd1u_=i!$lerdwabm+4t&&s7fh=j7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.ndnwckkbqm.ap-northeast-2.elasticbeanstalk.com']
 
 
 # Application definition
